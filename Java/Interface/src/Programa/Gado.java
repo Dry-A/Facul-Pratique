8228c6e36b5c144f5f8457e837fa8c16c0acb469
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-02-24 19:23:32 -0300

    Entrada de Dados gráficos

---
+++ Java/Interface/src/Programa/Gado.java
@@ -0,0 +1,4 @@
+package Programa;
+
+public class Gado {
+}
---
+++ Java/Interface/src/Programa/InterfaceAnimal.java
@@ -0,0 +1,8 @@
+package Programa;
+
+public interface InterfaceAnimal {
+
+    public void animalSom();
+    public void animalComer();
+    public void animalAbrigo();
+}
